983c77b2521c05eca777647e98c5b6121e655563
GNYAR <s901121s901121@gmail.com>
2022-10-26 13:35:29 +0800

    Initial Commit

---
+++ Random/RandomApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomApp.swift
+//  Random
+//
+//  Created by User20 on 2022/10/26.
+//
+
+import SwiftUI
+
+@main
+struct RandomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
